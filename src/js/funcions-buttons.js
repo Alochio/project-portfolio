c3547295a3a4a5214f0e4e439af4dbacd44a1459
Alochio <vinicius.alochio@aluno.ufop.edu.br>
2024-03-15 22:37:13 -0300

    button github e linkedin prontos

---
+++ src/js/funcions-buttons.js
@@ -0,0 +1,16 @@
+var buttonGit = document.querySelectorAll(".github");
+var buttonLinkedin = document.querySelectorAll(".linkedin");
+
+buttonGit.forEach(function (botao) {
+  botao.addEventListener("click", function () {
+    window.open("https://github.com/Alochio", "_blank");
+  });
+});
+
+
+buttonLinkedin.forEach(function (botao) {
+    botao.addEventListener("click", function () {
+      window.open("https://www.linkedin.com/in/vinicius-alochio/", "_blank");
+    });
+  });
+  
